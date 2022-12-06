58669dbe83f0ee04a557c37d8d6ff1d0882d71a7
ravi-kiran-k <114012879+ravi-kiran-k@users.noreply.github.com>
2022-12-06 21:58:22 +0530

    Add files via upload

---
+++ DDL Statements.sql
@@ -0,0 +1,47 @@
+-- Creating database
+create database RAVI;
+
+-- Use Database
+use ravi;
+
+-- Create Table
+create table Kiran(
+ID int not null,
+NAME varchar(32) not null,
+time timestamp default current_timestamp not null,
+AGE int,
+ADress varchar(32),
+Salary int
+);
+
+-- Describing the table
+desc customer;
+
+-- Changing the table name
+alter table kiran RENAME to customer;
+
+-- Add Primary key
+alter table customer add constraint primary key(ID);
+
+-- Add a new column
+alter table customer add column employer varchar(32);
+
+-- Dropping column 
+alter table customer drop column employer;
+
+-- dropping  table
+create table Kiran(
+ID int not null,
+NAME varchar(32) not null,
+time timestamp default current_timestamp not null,
+AGE int,
+ADress varchar(32),
+Salary int
+);
+drop table kiran;
+
+
+-- dropping database
+create database kiran;
+drop database kiran;
+
