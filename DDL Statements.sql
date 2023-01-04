91a0dda20f5b7130c9714d6d377f4e984ca2c63a
ravi-kiran-k <114012879+ravi-kiran-k@users.noreply.github.com>
2023-01-04 22:39:45 +0530

    Update DDL Statements.sql

--- DDL Statements.sql
+++ DDL Statements.sql
@@ -29,28 +29,5 @@ alter table customer add column employer varchar(32);
 -- Dropping column 
 alter table customer drop column employer;
 
--- dropping  table
-create table Kiran(
-ID int not null,
-NAME varchar(32) not null,
-time timestamp default current_timestamp not null,
-AGE int,
-ADress varchar(32),
-Salary int
-);
-drop table kiran;
-
-
--- dropping database
-create database kiran;
-drop database kiran;
-
-
---DML 
---INSERTING DATA Into a table
-insert into table kiran (values)
--- cte
-select * from ravi 
-where id like'%rav%';
 
 
